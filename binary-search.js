02fc84165e8ed6a0b54f36e7a4d370170814aa70
lucifer <azl397985856@gmail.com>
2021-07-01 18:04:43 +0800

    Create binary-search.js

---
+++ binary-search.js
@@ -0,0 +1,36 @@
+
+
+function bisect_left(nums, x) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] >= x) right = mid - 1;
+    if (nums[mid] < x) left = mid + 1;
+  }
+
+  return left;
+}
+
+function bisect_right(nums, x) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] > x) right = mid - 1;
+    if (nums[mid] <= x) left = mid + 1;
+  }
+
+  return left;
+}
+
+// test:
+
+bisect_left([1,2,5], 4) // 2
+bisect_right([1,2,5], 4) // 2
+bisect_left([1,2,5], 2) // 1
+bisect_right([1,2,5], 2) // 2
